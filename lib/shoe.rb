e24428d9fd4487b75dc0b416965a783fb245b0a6
Daniel Kim <daniel.kim52@gmail.com>
2020-04-22 17:28:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,15 @@
 # Make your shoe class here!
 class Shoe
 
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
+    condition = "new"
   end
 
 end
